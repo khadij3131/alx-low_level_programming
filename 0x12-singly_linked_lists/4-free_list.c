1af1079b12694438033c7227ad32774aacd336e1
khadij3131 <khadij-32@hotmail.com>
2023-10-19 10:13:03 -0700

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,46 +1,20 @@
 #include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list
- *@head: pointer to the head of the list
- *@str: string to be added to the list
+ *free_list - Frees a list
+ *@head: A pointer to the list head
  *
- *Return: NULL in case of failure
- *or address or the element
-*/
+ */
 
-list_t *add_node_end(list_t **head, const char *str)
+void free_list(list_t *head)
 {
-	char *dup;
-	int len;
-	list_t *new, *last;
+	list_t *tmp;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (str == NULL)
+	while (head)
 	{
-		free(new);
-		return (NULL);
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
 	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
-	}
-	return (*head);
 }
