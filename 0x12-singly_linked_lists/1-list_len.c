c3293d7a2857f1435c7a6f3bb3e967993e9630d2
khadij3131 <khadij-32@hotmail.com>
2023-10-19 09:46:17 -0700

    1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,21 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-
 /**
-* list_len - check the code for ALX School students.
-* @h: name of the list
-* Return: the number of nodes.
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
+ *
+ *Return: the number of elements
 */
+
 size_t list_len(const list_t *h)
-{
-   int count = 0;
+	{
+		size_t elements = 0;
 
+		while (h)
+		{
+			elements++;
+			h = h->next;
+		}
 
-   while (h)
-   {
-       count++;
-       h = h->next;
-   }
-   return (count);
-}
+		return (elements);
+	}
